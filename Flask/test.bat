e8034b8887a9966731e5271c2c48e11584500bed
vande202 <31896879+vande202@users.noreply.github.com>
2018-10-15 15:34:16 -0400

    Update test.bat

--- Flask/test.bat
+++ Flask/test.bat
@@ -3,9 +3,8 @@ title  Get er Done
 echo initiating flask (:
 
 
-set FLASK_APP=app.py
-set FLASK_ENV=development
-flask init-db
+set FLASK_APP=app
+set FLASK_ENV=developments
 start "" http://127.0.0.1:5000/
 flask run
 
