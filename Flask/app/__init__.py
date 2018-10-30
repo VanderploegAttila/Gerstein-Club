d37c0f91d555e187f8e6b19d42981f3af0cb73f9
vande202 <31896879+vande202@users.noreply.github.com>
2018-10-30 17:17:02 -0400

    help

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    return render_template('testindex.html')
 
 
 @app.route('/bathroom/', methods=['POST'])
