9664b5ae2eac541dda21a421431444831d55538c
vande202 <31896879+vande202@users.noreply.github.com>
2018-11-03 14:31:40 -0400

    help

--- Flask/app/helper.py
+++ Flask/app/helper.py
@@ -3,16 +3,17 @@ def gettestAOM():
 
 
 
-    backmap2={
+    bigmap2={
         'name': "backmap21",
-        'src':"static/images/badmap.gif",
+        'src':"static/images/bigmap2.svg",
         "stack":1,
-        'width':30,
-        'x_cord':5,
-        'y_cord': 30
+        'width':200,
+        'height':200,
+        'x_cord':1,
+        'y_cord': 1
     }
 
     #AOM.append(whomst)
     #AOM.append(backmap)
-    AOM.append(backmap2)
+    AOM.append(bigmap2)
     return AOM
